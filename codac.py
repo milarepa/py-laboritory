0fa1428bdc163577bcf4b27858c47d7221928802
milarepa <resonancetech@gmail.com>
2013-11-04 09:38:41 -0800

    move functions

--- codac.py
+++ codac.py
@@ -1,13 +1,3 @@
 #! /usr/bin/python
-def median(x):
-  lst = sorted(x)
-  length = len(lst)
-    
-  if length % 2 == 0:
-    a = lst[length / 2 - 1]
-    b = lst[length / 2]
-    return (a + b) / 2.0
-  else:
-    a = lst[int(length / 2.0 - 0.5)]
-    return a
+
     
--- pytools.py
+++ pytools.py
@@ -3,11 +3,26 @@ from random import randint
 # generate random lists...
 # little tools to help work exercises
 
+# generate a random list - more or less between 1 and 10
 def gen_list():
   lst = []
   for i in range(1, randint(1,10)):
     lst.append(randint(1, i))
   return lst
 
+# unneeded sort function :)
 def sort_list(x):
   return sorted(x)
+
+# median of a list
+def median(x):
+  lst = sorted(x)
+  length = len(lst)
+    
+  if length % 2 == 0:
+    a = lst[length / 2 - 1]
+    b = lst[length / 2]
+    return (a + b) / 2.0
+  else:
+    a = lst[int(length / 2.0 - 0.5)]
+    return a
